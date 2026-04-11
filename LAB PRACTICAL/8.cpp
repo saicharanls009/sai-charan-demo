9fb7df4a85f5855ebddfc65a4a2c83e7dfda2c33
SAI CHARAN <saicharanls999@gmail.com>
2026-04-11 11:20:37 +0530

    Update 8.cpp

--- LAB PRACTICAL/8.cpp
+++ LAB PRACTICAL/8.cpp
@@ -1,1 +1,19 @@
-//Write a C++ Program to demonstrate the Concept of Destructor.
+#include<iostream>
+using namespace std;
+
+class Demo{
+    public:
+    Demo(){
+    cout << "Constructor is called: object Created:"<<endl;
+}
+~Demo(){
+    cout << "Destructor is called : object Destroyed:"<<endl;
+}
+
+};
+int main(){
+    cout << "Creating Object.. \n";
+    Demo obj;
+    cout << "Exiting Program.. \n";
+    return 0;
+}
